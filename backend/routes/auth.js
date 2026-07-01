fdd3dccc6791b6d40ee12a951b821d4bcf347854
Vaidehi <vaidehikumawat2020@gmail.com>
2026-07-01 15:29:34 +0530

    Fix User.js import for Linux deployment

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
